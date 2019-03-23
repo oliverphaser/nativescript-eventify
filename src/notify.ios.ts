d0599b391c233c7100478ad59ab8928ce8312c1c
Mudlabs_Monster <samgdonald+mudlabs@gmail.com>
2019-03-23 15:42:40 +1100

    Set the method eventify to the ViewBase prototype in ios

--- src/notify.ios.ts
+++ src/notify.ios.ts
@@ -1,5 +1,22 @@
-/**
- * User passes in  the object { eventName: string, object: View }
- */
+import * as Common from "./notify.common";
+import { ViewBase } from "tns-core-modules/ui/core/view-base";
 
-const notify = object => (object.ios.gestureRecognizers[0].state = 1);
+declare const UIGestureRecognizerStateEnded: number;
+
+//@ts-ignore
+ViewBase.prototype.eventify = function(data: Common.EventData): void {
+  Common.canBeHandledByNotify(data)
+    ? data.object.notify(data)
+    : () => {
+        const index = [...data.object.ios.gestureRecognizers].findIndex(
+          gesture => gesture.numberOfTapsRequired
+        );
+
+        /**
+       * The gesture recognizer has received touches recognized as the end of a continuous gesture. It sends its action message (or messages) at the next cycle of the run loop and resets its state to UIGestureRecognizer.State.possible.
+      */
+        data.object.ios.gestureRecognizers[
+          index
+        ].state = UIGestureRecognizerStateEnded;
+      };
+};

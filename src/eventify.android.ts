cd56786de883729c25daa6f6619e79b3d095fd42
Mudlabs_Monster <samgdonald+mudlabs@gmail.com>
2019-03-27 10:22:43 +1100

    Enabled basic tap events for Android

--- src/eventify.android.ts
+++ src/eventify.android.ts
@@ -1,40 +1,70 @@
 import * as Common from "./eventify.common";
 import { ViewBase } from "tns-core-modules/ui/core/view-base";
 
+/**
+ * Simulates a pointer touching down onto the device.
+ */
+const touchDown = view => {
+  const origin = Common.getObjectXYRelativeToPage(view);
+  view.android.dispatchTouchEvent(
+    android.view.MotionEvent.obtain(
+      android.os.SystemClock.uptimeMillis(),
+      android.os.SystemClock.uptimeMillis(),
+      android.view.MotionEvent.ACTION_DOWN,
+      origin.x + view.getActualSize().width / 2,
+      origin.y + view.getActualSize().height / 2,
+      0
+    )
+  );
+};
+
+/**
+ * Simulates a pointer raising up from the device.
+ */
+const touchUp = veiw => {
+  const origin = Common.getObjectXYRelativeToPage(veiw);
+  veiw.android.dispatchTouchEvent(
+    android.view.MotionEvent.obtain(
+      android.os.SystemClock.uptimeMillis(),
+      android.os.SystemClock.uptimeMillis(),
+      android.view.MotionEvent.ACTION_UP,
+      origin.x + veiw.getActualSize().width / 2,
+      origin.y + veiw.getActualSize().height / 2,
+      0
+    )
+  );
+};
+
+/**
+ * Simulates a tap event.
+ * 
+ * @param view - The object to be touched
+ */
+const tap = view => {
+  touchDown(view);
+  touchUp(view);
+};
+
+/**
+ * Add the eventify method to the ViewBase prototype chain so it's available to everything that extends from it.
+ * 
+ * @param data - The `eventName` and `object` that would otherwise have been passed to `notify(...)`.
+ * @param info - The additional information needed to execute the event.
+ * 
+ * @returns void
+ */
 //@ts-ignore
-ViewBase.prototype.eventify = function(data: Common.EventData): void {
-  const object = data.object;
-  const eventName = data.eventName;
-
-  const getCoordinateWithAxisAndDimention = (axis: string, dimension: string) =>
-    this.object.getLocationInWindow()[axis] -
-    this.object.getActualSize()[dimension] / 2;
-
-  Common.canBeHandledByNotify(data)
-    ? data.object.notify(data)
-    : () => {
-        // Simulates a pointer touching down onto the device.
-        this.object.android.dispatchTouchEvent(
-          android.view.MotionEvent.obtain(
-            android.os.SystemClock.uptimeMillis(),
-            android.os.SystemClock.uptimeMillis(),
-            android.view.MotionEvent.ACTION_DOWN,
-            this.getCoordinateWithAxisAndDimention("x", "width"),
-            this.getCoordinateWithAxisAndDimention("y", "height"),
-            0
-          )
-        );
-
-        // Simulates a ponter raising up from the device.
-        this.object.android.dispatchTouchEvent(
-          android.view.MotionEvent.obtain(
-            android.os.SystemClock.uptimeMillis(),
-            android.os.SystemClock.uptimeMillis(),
-            android.view.MotionEvent.ACTION_UP,
-            this.getCoordinateWithAxisAndDimention("x", "width"),
-            this.getCoordinateWithAxisAndDimention("y", "height"),
-            0
-          )
-        );
-      };
+ViewBase.prototype.eventify = function(
+  data: Common.EventData,
+  info: Common.EventInfo
+): void {
+  Common.canBeHandledByNotify(this, data.eventName)
+    ? this.notify(data)
+    : (() => {
+        switch (data.eventName) {
+          case "tap":
+            tap(this);
+            break;
+        }
+      })();
 };

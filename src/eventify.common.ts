78432b35dcff1d49bf9337167fe3615e74f57b39
Mudlabs_Monster <samgdonald+mudlabs@gmail.com>
2019-03-27 10:24:45 +1100

    Use topmost to get location of tappable views. Add eventify_events array. Add EventInfo interface object. Reformat canBeHandledByNotify to examin ViewBase in question.

--- src/eventify.common.ts
+++ src/eventify.common.ts
@@ -1,9 +1,17 @@
-import { View } from "tns-core-modules/ui/core/view";
+import { topmost } from "tns-core-modules/ui/frame";
 import { getBaseClasses } from "tns-core-modules/utils/types";
+
+// Add supported event names here.
+const eventify_events = ["tap"];
 
 export interface EventData {
   eventName: string;
-  object: View;
+  object: object;
+}
+
+export interface EventInfo {
+  press?: number;
+  direction?: "left" | "up" | "right" | "down";
 }
 
 /**
@@ -11,8 +19,28 @@ export interface EventData {
  * 
  * Right now we only deal with tap events so if the eventName is not "tap" or it is but the object is a Button then we return true and the object notify method will handle it.
  * 
- * @param data - the eventName and object handed to notify(...);
+ * @param viewBase - The ViewBase raising the event in question.
+ * @param eventName - The name of the requested event.
  * 
+ * @returns boolean
  */
-export const canBeHandledByNotify = (data: EventData) =>
-  data.eventName !== "tap" || getBaseClasses(data.object)[0] === "Button";
+export const canBeHandledByNotify = (viewBase, eventName) => {
+  if (
+    !eventify_events.includes(eventName) ||
+    getBaseClasses(viewBase).includes("Button")
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Returns the x and y origin values for the view relative to the current page.
+ * 
+ * @param view - The View we want the location of.
+ * 
+ * @returns { x: number, y: number }
+ */
+export const getObjectXYRelativeToPage = view =>
+  view.getLocationRelativeTo(topmost().currentPage);

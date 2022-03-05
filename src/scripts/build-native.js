d9ff9178465d9651ac208f294e827548b7261da1
PHASER <phaser@radioimpex.net>
2022-03-05 17:56:15 +0300

    Update to NS7

--- src/scripts/build-native.js
+++ src/scripts/build-native.js
@@ -12,7 +12,7 @@ exec('ns --version', (err, stdout, stderr) => {
 
     // execute 'ns plugin build' for {N} version > 8. This command builds .aar in platforms/android folder.
     if (tnsVersion >= 8) {
-        console.log(`executing 'tns plugin build'`);
+        console.log(`executing 'ns plugin build'`);
         exec('ns plugin build', (err, stdout, stderr) => {
             if (err) {
                 // node couldn't execute the command
